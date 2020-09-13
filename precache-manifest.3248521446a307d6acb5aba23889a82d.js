0e60d4151b46593c4d5f37dec46a9bcb5ff3efd8
Shiran Meir <shiranmeir7@gmail.com>
2020-09-13 16:08:58 +0300

    Updates

---
+++ precache-manifest.3248521446a307d6acb5aba23889a82d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "383881a61a61c3b883e801078c7f05fa",
+    "url": "/TEAM/index.html"
+  },
+  {
+    "revision": "590b97713e1d7e418e05",
+    "url": "/TEAM/static/css/2.d4e7eff5.chunk.css"
+  },
+  {
+    "revision": "e4729b63096bb1a5bdb9",
+    "url": "/TEAM/static/css/main.05f37cff.chunk.css"
+  },
+  {
+    "revision": "590b97713e1d7e418e05",
+    "url": "/TEAM/static/js/2.560bd5a0.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/TEAM/static/js/2.560bd5a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4729b63096bb1a5bdb9",
+    "url": "/TEAM/static/js/main.756df9bb.chunk.js"
+  },
+  {
+    "revision": "a5421c6690028fc17a36",
+    "url": "/TEAM/static/js/runtime-main.e36827c0.js"
+  }
+]);
